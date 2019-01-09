1512aa9426b90943460c7474d1d666b4a4bf3a39
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2019-01-08 20:42:57 -0700

    files created, stuff is on page but no functionality

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Buffalo and Bleu Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Turkey and Provolone Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (name, eaten) VALUES ('Lamb and Rosemary Burger', true);
+INSERT INTO burgers (name) VALUES ('Meatless Cheeseburger, AKA Grilled Cheese');
+INSERT INTO burgers (name) VALUES ('Pulled Pork Sammich');
